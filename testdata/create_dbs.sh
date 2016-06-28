d251422a5d9c71131e96004e4300490b675f1a33
AsgerPetersen <asgerpetersen@gmail.com>
2016-06-28 14:12:39 +0200

    Reenable create_dbs.sh

--- testdata/create_dbs.sh
+++ testdata/create_dbs.sh
@@ -13,17 +13,19 @@ echo 607_Fredericia_fot5_fra_prod
 sed 's/posList>/posList srsDimension="3">/' 607_Fredericia_fot5_fra_prod.gml > tmp1.gml
 ogr2ogr -f SQLite -gt 65536 -lco SPATIAL_INDEX=YES fot5.sqlite tmp1.gml
 
-#rm tmp.gml
-#rm tmp.gfs
+rm tmp1.gml
+rm tmp1.gfs
 
 echo Mapped_Fredericia_original_fot_5
 sed 's/posList>/posList srsDimension="3">/' Mapped_Fredericia_original_fot_5.gml > tmp2.gml
 ogr2ogr -f SQLite -gt 65536 -lco SPATIAL_INDEX=YES mapped_fot4.sqlite tmp2.gml
 
-#rm tmp.gml
-#rm tmp.gfs
+rm tmp2.gml
+rm tmp2.gfs
 
-#rm 607_Fredericia_fot5_fra_prod.gml
-#rm Mapped_Fredericia_original_fot_5.gml
+rm 607_Fredericia_fot5_fra_prod.gml
+rm Mapped_Fredericia_original_fot_5.gml
+
+rm -rf ./__MACOSX
 
 # xmllint --noout --schema ./schemas/gml/3.1.1/base/gml.xsd 461_Odense_Rev3_20160310/607_Fredericia_fot5_fra_prod.gml
