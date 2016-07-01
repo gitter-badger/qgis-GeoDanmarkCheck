8537e864ddb5995f19065c87231ef8f7c216aedd
AsgerPetersen <asgerpetersen@gmail.com>
2016-07-01 14:02:24 +0200

    FeatureMatcher

--- GeoDanmarkChecker/fot/geomutils/featurematcher.py
+++ GeoDanmarkChecker/fot/geomutils/featurematcher.py
@@ -1,45 +1,87 @@
-from GeoDanmarkChecker.fot.geomutils import FeatureIndex, togeometry
-from qgis.core import QgsGeometry, QgsGeometryEngine
-# Class which matches two features against each other
+from . import FeatureIndex, togeometry, geometryequal
+from qgis.core import QgsGeometry, QgsGeometryEngine, QgsFeature
 
+# Classes which finds matching features in another collection of features
 
-class FeatureMatcher(object):
-
-    def __init__(self):
-        # exact geometry match
-        # at least some intersection
-        # centroid inside
-        # within distance
-        self.coordinatetolerance = 0.01
-        pass
-
-
-    def match(self, featurecollection1, featurecollection2):
-        pass
-        yield match
-
-
-    def _oneagainstmany(self,f, indexedfeaturecollection):
-        if not isinstance(indexedfeaturecollection, FeatureIndex):
-            raise TypeError("Expected FeatureIndex")
-        geom = togeometry(f)
-
-
-        intersecting = indexedfeaturecollection.geometryintersects(f)
-        for otherfeature in intersecting:
-            othergeom = togeometry(otherfeature)
-            if QgsGeometry.compare(f, othergeom, self.coordinatetolerance):
-                return (f, otherfeature, geom)
-
-            geomengine = QgsGeometryEngine( geom.geometry() )
-            geomengine.prepareGeometry()
-
-
-
-
+class PreparedFeature(object):
+    def __init__(self, feature):
+        self.feature = feature
+        self.geom = togeometry(feature)
+        geomengine = QgsGeometry.createGeometryEngine(self.geom.geometry())
+        geomengine.prepareGeometry()
+        self.preparedgeom = geomengine
 
 class FeatureMatch(object):
     def __init__(self, f1, f2, matchgeom = None):
         self.feature1 = f1
         self.feature2 = f2
         self.matchgeometry = matchgeom
+
+class FeatureMatcher(object):
+
+    def __init__(self, coordinatetolerance = 0.01, bboxexpansion = 0.0):
+        self.coordinatetolerance = coordinatetolerance
+        self.bboxexpansion = bboxexpansion
+
+    def match(self, featurecollection1, featurecollection2):
+        indexedcollection2 = FeatureIndex(featurecollection2, usespatialindex=True)
+        for f in featurecollection1:
+            prep = PreparedFeature(f)
+            for m in self._oneagainstmany(prep, indexedcollection2):
+                yield m
+
+    def _oneagainstmany(self, prep, indexedfeaturecollection):
+        if not isinstance(indexedfeaturecollection, FeatureIndex):
+            raise TypeError("Expected FeatureIndex")
+
+        geom = prep.geom
+        bbox = geom.boundingBox()
+        if self.bboxexpansion > 0:
+            bbox.buffer(self.bboxexpansion)
+
+        intersecting = indexedfeaturecollection.geometryintersects(bbox)
+        for otherfeature in intersecting:
+            othergeom = togeometry(otherfeature)
+            if geometryequal(geom, othergeom, self.coordinatetolerance):
+                yield FeatureMatch(prep.feature, otherfeature, geom)
+            else:
+                m = self.matchesfeature(prep, otherfeature)
+                if m:
+                    yield m
+
+class LineMatcher(FeatureMatcher):
+    pass
+
+
+class PolygonMatcher(FeatureMatcher):
+
+    def __init__(self, **kwargs):
+        super(PolygonMatcher, self).__init__()
+
+        self.useareaintersection = False
+        if 'relativeareadeviation' in kwargs:
+            self.relativeareadeviation = float( kwargs['relativeareadeviation'] )
+            self.useareaintersection = bool(self.relativeareadeviation)
+
+    def matchesfeature(self, preparedfeature, otherfeature):
+        othergeom = togeometry(otherfeature)
+        if self.useareaintersection:
+            area1 = preparedfeature.preparedgeom.area()
+            area2 = othergeom.area()
+            intersection = preparedfeature.preparedgeom.intersection(othergeom.geometry())
+            intersectionarea = intersection.area()
+            diff1 = abs(area1 - intersectionarea) / area1
+            diff2 = abs(area2 - intersectionarea) / area2
+            if diff1 < self.relativeareadeviation or diff2 < self.relativeareadeviation:
+                return FeatureMatch(preparedfeature.feature, otherfeature, intersection)
+        return None
+
+class PointMatcher(FeatureMatcher):
+    pass
+
+
+
+
+
+
+
