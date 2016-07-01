f1a4cd16222a5b617ccb36e647c364ce64c70fdf
AsgerPetersen <asgerpetersen@gmail.com>
2016-07-01 14:27:34 +0200

    KOmmentar på reporter

--- GeoDanmarkChecker/fot/reporter.py
+++ GeoDanmarkChecker/fot/reporter.py
@@ -7,6 +7,7 @@ class Reporter:
     def __init__(self, outputfile):
         self.outputfile = outputfile
 
+    # Maybe take a Rule as first argument? Rule knows its name and should now what types are touched
     def reportError(self, rulename, typeinfo, message, geometry):
         self.report(rulename, typeinfo, message, geometry, "error")
 
