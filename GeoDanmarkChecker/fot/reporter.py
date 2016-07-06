c62e4e5704e8219b28b88f8f18029e7f3316abc2
Stephan Telling <st@telling.xyz>
2016-07-06 13:32:43 +0200

    Report to spatialite

--- GeoDanmarkChecker/fot/reporter.py
+++ GeoDanmarkChecker/fot/reporter.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from PyQt4.QtCore import QVariant
 from qgis.core import (
     QgsFeature,
     QGis
 )
 import osgeo.ogr as ogr
-import osgeo.osr as osr
+from ogrspatialite import OGRSPatialite
 
 
 class Reporter(object):
@@ -17,7 +16,7 @@ class Reporter(object):
         self.feature = None
         self.data_source = None
         self.output_path = output_path
-        self._initialize_spatialite()
+        self._initialize_db()
 
     def reportError(self, rulename, typeinfo, message, geometry):
         self.report(rulename, typeinfo, message, geometry, "error")
@@ -28,49 +27,57 @@ class Reporter(object):
     def report(self, rulename, typeinfo, message, geometry, level):
         if isinstance(geometry, QgsFeature):
             geometry = geometry.geometry()
+            fields = [
+                ('rulename', str(rulename)),
+                ('typeinfo', str(typeinfo)),
+                ('message', str(message)),
+                ('level', str(level))
+            ]
             # QGis.flatType if we don't care about 25D and whatnot
             if QGis.flatType(geometry.wkbType()) == QGis.WKBLineString:
-                self.feature = ogr.Feature(self.layer.GetLayerDefn())
-                self.feature.SetField("rulename", str(rulename))
-                self.feature.SetField("typeinfo", str(typeinfo))
-                self.feature.SetField("message", str(message))
-                self.feature.SetField("level", str(level))
-                self.feature.SetGeometry(
-                    ogr.CreateGeometryFromWkb(geometry.asWkb())
+                self.db.add_feature_to_layer(
+                    'linestring',
+                    fields,
+                    geometry
                 )
-                self.layer.CreateFeature(self.feature)
-                self.feature.Destroy()
 
             if QGis.flatType((geometry.wkbType())) == QGis.WKBPoint:
-                pass
+                self.db.add_feature_to_layer(
+                    'point',
+                    fields,
+                    geometry
+                )
 
             if QGis.flatType((geometry.wkbType())) == QGis.WKBPolygon:
-                pass
+                self.db.add_feature_to_layer(
+                    'polygon',
+                    fields,
+                    geometry
+                )
 
-    def _initialize_spatialite(self):
-        self.driver = ogr.GetDriverByName("SQLite")
-        self.data_source = self.driver.CreateDataSource(
-            "geodk_report_{}.sqlite".format(
-                datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
-            ),
-            options=[
-                'SPATIALITE=YES',
-                'INIT_WITH_EPSG=YES',
-                'OGR_SQLITE_SYNCHRONOUS=OFF'
-            ]
+    def _initialize_db(self):
+        self.db = OGRSPatialite(self.output_path)
+        fields = [
+            ('rulename', ogr.OFTString),
+            ('typeinfo', ogr.OFTString),
+            ('message', ogr.OFTString),
+            ('level', ogr.OFTString)
+        ]
+        # Add layer for linestring
+        self.db.add_layer(
+            'linestring',
+            ogr.wkbLineString25D,
+            fields
         )
-        srs = osr.SpatialReference()
-        srs.ImportFromEPSG(25832)
-        self.layer = self.data_source.CreateLayer("linestring_report", srs, ogr.wkbLineString25D)
-        rulename = ogr.FieldDefn("rulename", ogr.OFTString)
-        rulename.SetWidth(1000)
-        self.layer.CreateField(rulename)
-        typeinfo = ogr.FieldDefn("typeinfo", ogr.OFTString)
-        typeinfo.SetWidth(1000)
-        self.layer.CreateField(typeinfo)
-        message = ogr.FieldDefn("message", ogr.OFTString)
-        message.SetWidth(1000)
-        self.layer.CreateField(message)
-        level = ogr.FieldDefn("level", ogr.OFTString)
-        level.SetWidth(1000)
-        self.layer.CreateField(level)
+        # Add layer for points
+        self.db.add_layer(
+            'point',
+            ogr.wkbPoint,
+            fields
+        )
+        # Add layer for polygons
+        self.db.add_layer(
+            'polygon',
+            ogr.wkbPolygon,
+            fields
+        )
