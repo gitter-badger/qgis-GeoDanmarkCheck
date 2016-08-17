16f8b772b7544701b6c59b22f6fc70c51a275cd1
AsgerPetersen <asgerpetersen@gmail.com>
2016-08-17 11:33:19 +0200

    Make PreliminaryObjectsRule more configurable

--- GeoDanmarkChecker/fot/rules/dualdataset/preliminaryobjects.py
+++ GeoDanmarkChecker/fot/rules/dualdataset/preliminaryobjects.py
@@ -20,17 +20,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .comparerule import CompareRule
 from ...repository import Repository
-from ...geomutils import FeatureIndex, geometryequal, shortestline
+from ...geomutils import FeatureIndex, geometryequal, shortestline, changedattributes
+from ...geomutils.featurematcher import MatchFinder
 
 
 class PreliminaryObjectsRule(CompareRule):
 
-    def __init__(self, name, feature_type):
+    def __init__(self, name, feature_type, ispreliminaryfunction, nearbymatcher):
         super(PreliminaryObjectsRule, self).__init__(name)
-        self.statusattribute = u'geometri_status'
         self.featuretype = feature_type
-        self.beforefilter = self.statusattribute + u' = "Foreløbig"'
         self.idcolumn = self.featuretype.id_attribute
+
+        if not hasattr(ispreliminaryfunction, '__call__'):
+            raise TypeError("ispreliminary function must be a function")
+        self.ispreliminary = ispreliminaryfunction
+        self.nearbymatcher = nearbymatcher
 
     def execute(self, beforerepo, afterrepo, errorreporter, progressreporter):
         if not isinstance(beforerepo, Repository):
@@ -38,32 +42,76 @@ class PreliminaryObjectsRule(CompareRule):
         if not isinstance(afterrepo, Repository):
             raise TypeError()
 
-        neededattribs = [self.idcolumn, self.statusattribute]
+        beforefeats = beforerepo.read(self.featuretype)
+        afterfeats = afterrepo.read(self.featuretype)
 
-        beforefeats = beforerepo.read(self.featuretype, attributes=neededattribs, feature_filter=self.beforefilter)
-        afterfeats = afterrepo.read(self.featuretype, attributes=neededattribs)
+        preliminary_objects = [x for x in beforefeats if self.ispreliminary(x)]
 
-        progressreporter.begintask(self.name, len(beforefeats))
+        progressreporter.begintask(self.name, len(preliminary_objects))
 
-        if not beforefeats:
-            return
+        beforeindex = FeatureIndex(beforefeats, usespatialindex=False, indexedattributes=[self.idcolumn])
+        afterindex = FeatureIndex(afterfeats, usespatialindex=False, indexedattributes=[self.idcolumn])
+        nearbyfinder = MatchFinder(afterfeats)
 
-        afterindex = FeatureIndex(afterfeats, usespatialindex=False, indexedattributes=[self.statusattribute])
+        for f in preliminary_objects:
+            progressreporter.completed_one() # At the beginning because of the continues
 
-        for f in beforefeats:
             fot_id = f[self.idcolumn]
-            matches = afterindex.attributeequals(self.idcolumn, fot_id)
+            prelim_after = afterindex.attributeequals(self.idcolumn, fot_id)
 
-            if not matches:
-                errorreporter.error(self.name, self.featuretype, 'Preliminary object ({0}={1}) deleted'.format(self.idcolumn, fot_id), f)
+            # Does the prelim object exist in the "after" dataset
+            isdeleted = len(prelim_after) == 0
 
-            # Should be checked elsewhere
-            if len(matches) > 1:
-                line = shortestline(matches[0], matches[1])
-                errorreporter.error(self.name, self.featuretype, 'Multiple objects  with {0}={1}'.format(self.idcolumn, fot_id), line)
+            # Is it still preliminiary
+            if not isdeleted:
+                if self.ispreliminary(prelim_after[0]):
+                    # was anything changed?
+                    if not geometryequal(f, prelim_after[0]):
+                        errorreporter.error(
+                            self.name,
+                            self.featuretype,
+                            'Preliminary object ({0}={1}) geometry changed but still preliminary'.format(self.idcolumn, fot_id),
+                            f)
+                    changed_attributes = changedattributes(f, prelim_after[0])
+                    if changed_attributes:
+                        errorreporter.error(
+                            self.name,
+                            self.featuretype,
+                            'Preliminary object ({0}={1}) attributes changed but still preliminary'
+                                .format(self.idcolumn, fot_id),
+                            f)
 
-            newf = matches[0]
+            # Are there any objects of the same type "nearby"
+            nearby = nearbyfinder.findmatching(f, self.nearbymatcher)
 
-            geometryequals = geometryequal(f, newf)
-            # TODO: What are the rules exactly?
+            # Check if one of the nearby objects is newly registered
+            new_object = None
+            for match in nearby:
+                # Is idcolumn==None => object is new
+                matching_feature = match.feature2
+                if matching_feature[self.idcolumn] is None:
+                    new_object = matching_feature
+                    break
+                # Is idcolumn not found in the before dataset
+                sameids = beforeindex.attributeequals(self.idcolumn, matching_feature[self.idcolumn])
+                if len(sameids) == 0:
+                    new_object = matching_feature
+                    break
+
+            if isdeleted and not new_object:
+                errorreporter.error(
+                    self.name,
+                    self.featuretype,
+                    'Preliminary object ({0}={1}) deleted without new object registered'.format(self.idcolumn, fot_id),
+                    f)
+                continue
+
+            if new_object and not isdeleted:
+                errorreporter.warning(
+                    self.name,
+                    self.featuretype,
+                    'Preliminary object ({0}={1}) not deleted but new object registered'.format(self.idcolumn, fot_id),
+                    shortestline(f, new_object))
+
+
 
