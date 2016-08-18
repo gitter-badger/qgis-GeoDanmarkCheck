8db86819444c1d149a29426882efcb4216600663
AsgerPetersen <asgerpetersen@gmail.com>
2016-08-18 21:33:47 +0200

    Fix angle threshold in segmentmatcher

--- GeoDanmarkChecker/fot/geomutils/segmentmatcher.py
+++ GeoDanmarkChecker/fot/geomutils/segmentmatcher.py
@@ -3,7 +3,7 @@ from . import FeatureIndex
 from featurematcher import FeatureMatch
 from . import togeometry, tocoordinates
 from .algorithms import densify
-from math import sqrt
+from math import sqrt, cos, pi
 
 class SegmentMatch(object):
     def __init__(self, f, nearest_feature):
@@ -24,6 +24,7 @@ class SegmentMatchFinder(object):
         self.features = matchagainstfeatures
         self.indexedfeatures = FeatureIndex(matchagainstfeatures, usespatialindex=True)
         self.segmentize = float(segmentize) if segmentize else 0
+        self.approximate_angle_threshold = cos(pi/4.0)
 
     def findmatching(self, feature, maxdistance):
         maxdistance = float(maxdistance)
@@ -54,9 +55,11 @@ class SegmentMatchFinder(object):
         for i in range(1, len(distances_per_vertex)):
             this_vertex = segcoords[i]
             prev_vertex = segcoords[i-1]
+            this_segment = QgsGeometry.fromPolyline([prev_vertex, this_vertex])
+            this_segment_length = this_segment.length()
             prev_distances = distances_per_vertex[i - 1]
             this_distances = distances_per_vertex[i]
-            smallest_distance = float('inf')
+            smallest_distance_sqrd = float('inf')
             smallest_distance_feature = None
             for f, this_distance in this_distances.iteritems():
                 if not f in prev_distances:
@@ -64,19 +67,20 @@ class SegmentMatchFinder(object):
                     continue
                 prev_distance = prev_distances[f]
                 sumsqrddistance = this_distance[0] + prev_distance[0]
-                # What point was closest? Make sure we dont match to an endpoint
+                # What point was closest? Make sure we dont match an endpoint of the other geom or a geom at a near 90 angle
                 prev_point = prev_distance[1]
                 this_point = this_distance[1]
-                if not prev_point == this_point and sumsqrddistance < smallest_distance:
+                other_segment = QgsGeometry.fromPolyline([prev_point, this_point])
+                if other_segment.length() > this_segment_length * self.approximate_angle_threshold and sumsqrddistance < smallest_distance_sqrd:
                     # We found a closer feature
-                    smallest_distance = sumsqrddistance
+                    smallest_distance_sqrd = sumsqrddistance
                     smallest_distance_feature = f
                     # Perfekt match. Stop looking
-                    if smallest_distance == 0.0:
+                    if smallest_distance_sqrd == 0.0:
                         break
             # Do we have a running segmentmatch to extend?
             if current_segmentmatch and current_segmentmatch.nearestfeature == smallest_distance_feature:
-                current_segmentmatch.addvertex(this_vertex, sqrt(smallest_distance))
+                current_segmentmatch.addvertex(this_vertex, sqrt(smallest_distance_sqrd))
             else:
                 # The closes feature is not the same as in the last segment
                 if current_segmentmatch:
@@ -84,8 +88,8 @@ class SegmentMatchFinder(object):
                     current_segmentmatch = None
                 if smallest_distance_feature:
                     current_segmentmatch = SegmentMatch(feature, smallest_distance_feature)
-                    current_segmentmatch.addvertex(prev_vertex, sqrt(smallest_distance))
-                    current_segmentmatch.addvertex(this_vertex, sqrt(smallest_distance))
+                    current_segmentmatch.addvertex(prev_vertex, sqrt(smallest_distance_sqrd))
+                    current_segmentmatch.addvertex(this_vertex, sqrt(smallest_distance_sqrd))
         # Add last segmentmatch
         if current_segmentmatch:
             segment_matches.append(current_segmentmatch)
