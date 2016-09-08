326fe60c6b13a9298a26e9e74855de4df6ca0646
AsgerPetersen <asgerpetersen@gmail.com>
2016-09-08 16:26:02 +0200

    Fix ‘changedattributes'

--- GeoDanmarkChecker/fot/geomutils/_featuretools.py
+++ GeoDanmarkChecker/fot/geomutils/_featuretools.py
@@ -19,16 +19,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 def attributenames(f):
-    [field.name() for field in f.fields()]
+    return [field.name() for field in f.fields()]
 
 def changedattributes(f1, f2, checkattributes=None):
     attriblist = checkattributes if checkattributes else attributenames(f1)
+    result = []
     for attrib in attriblist:
-        result = []
         try:
             if not f1[attrib] == f2[attrib]:
-                message = u'Attribute {0} changed from {1} to {2}'.format(attrib, f1[attrib], f2[attrib])
+                result.append((attrib, u'Attribute {0} changed from {1} to {2}'.format(attrib, f1[attrib], f2[attrib])))
         except KeyError as e:
-            message = u'Attribute {0} not found'.format(attrib)
-        if message:
-            result.append((attrib, message))
+            result.append((attrib, u'Attribute {0} not found'.format(attrib)))
