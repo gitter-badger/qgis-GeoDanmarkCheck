6cfd464b0b63e803e81cc19d61819ceea868326c
AsgerPetersen <asgerpetersen@gmail.com>
2016-09-08 16:26:44 +0200

    Dont crash featureindex when no trying to insert None or empty list

--- GeoDanmarkChecker/fot/geomutils/_tools.py
+++ GeoDanmarkChecker/fot/geomutils/_tools.py
@@ -43,33 +43,6 @@ def shortestline(f1, f2):
     g2 = togeometry(f2)
     return g1.shortestLine(g2)
 
-
-def bbox(f):
-    """Returns the bbox [xmin,ymin,xmax,ymax] for a feature, a geometry, a bbox or a list of those"""
-    if isinstance(f, QgsFeature) or isinstance(f, QgsGeometry):
-        g = togeometry(f)
-        rect = g.boundingBox()
-        return [rect.xMinimum(), rect.yMinimum(), rect.xMaximum(), rect.yMaximum()]
-
-    if isinstance(f, QgsRectangle):
-        return [f.xMinimum(), f.yMinimum(), f.xMaximum(), f.yMaximum()]
-
-    # Potentially a bbox or list of geoms or features
-    if isinstance(f, (list, tuple)) and len(f) == 4:
-        if all([isinstance(n, (int, float)) for n in f]):
-            # This is a bbox itself
-            return f
-    bboxes = [bbox(o) for o in f]
-    xmins, ymins, xmaxs, ymaxs = zip(*bboxes)
-    return [min(xmins), min(ymins), max(xmaxs), max(ymaxs)]
-
-
-def bboxoverlaps(f1, f2):
-    """Inputs are either QgsGeometry, QgsFeature or a bbox [xmin,ymin,xmax,ymax]"""
-    b1 = bbox(f1)
-    b2 = bbox(f2)
-    return b1[0] < b2[2] and b1[2] > b2[0] and b1[1] < b2[3] and b1[3] > b2[1]
-
 def geometryequal(f1, f2, tolerance = 0.01):
     """Compares geometries coordinate by coordinate using specified tolerance"""
     g1 = togeometry(f1)
--- GeoDanmarkChecker/fot/geomutils/featureindex.py
+++ GeoDanmarkChecker/fot/geomutils/featureindex.py
@@ -18,10 +18,36 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from qgis.core import QgsFeature
+from qgis.core import QgsFeature, QgsRectangle, QgsGeometry
 from collections import defaultdict
-from . import togeometry, bbox, bboxoverlaps
 from .pyqtree import Index
+from . import togeometry
+
+def bbox(f):
+    """Returns the bbox [xmin,ymin,xmax,ymax] for a feature, a geometry, a bbox or a list of those"""
+    if isinstance(f, QgsFeature) or isinstance(f, QgsGeometry):
+        g = togeometry(f)
+        rect = g.boundingBox()
+        return [rect.xMinimum(), rect.yMinimum(), rect.xMaximum(), rect.yMaximum()]
+
+    if isinstance(f, QgsRectangle):
+        return [f.xMinimum(), f.yMinimum(), f.xMaximum(), f.yMaximum()]
+
+    # Potentially a bbox or list of geoms or features
+    if isinstance(f, (list, tuple)) and len(f) == 4:
+        if all([isinstance(n, (int, float)) for n in f]):
+            # This is a bbox itself
+            return f
+    bboxes = [bbox(o) for o in f]
+    xmins, ymins, xmaxs, ymaxs = zip(*bboxes)
+    return [min(xmins), min(ymins), max(xmaxs), max(ymaxs)]
+
+
+def bboxoverlaps(f1, f2):
+    """Inputs are either QgsGeometry, QgsFeature or a bbox [xmin,ymin,xmax,ymax]"""
+    b1 = bbox(f1)
+    b2 = bbox(f2)
+    return b1[0] < b2[2] and b1[2] > b2[0] and b1[1] < b2[3] and b1[3] > b2[1]
 
 class FeatureIndex():
     # http://docs.qgis.org/testing/en/docs/pyqgis_developer_cookbook/vector.html#using-spatial-index
@@ -46,6 +72,8 @@ class FeatureIndex():
         return index[value]
 
     def insert(self, features):
+        if not features:
+            return
         if isinstance(features, QgsFeature):
             features = [features]
         if not all(isinstance(f, QgsFeature) for f in features):
