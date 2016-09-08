cb18ec046a62d23e6f03a4ffc12c912f039fcbf3
AsgerPetersen <asgerpetersen@gmail.com>
2016-09-08 16:40:21 +0200

    Fall back to simple errorgeom between linestrings

--- GeoDanmarkChecker/fot/geomutils/errorgeometry.py
+++ GeoDanmarkChecker/fot/geomutils/errorgeometry.py
@@ -1,5 +1,6 @@
 from . import togeometry, extractassingle, shortestline, tocoordinates
 from qgis.core import QGis, QgsGeometry
+from math import sqrt
 
 # Marker lines basically go to a point this far into the geometry
 MARKER_SHRINK_SIZE = 0.5
@@ -12,24 +13,28 @@ def linemarkerpoint(g, fromgeom=None):
     if g.type() == QGis.Point:
         return g
     elif g.type() == QGis.Line:
-        length = g.length()
-        if fromgeom and length > 2 * MARKER_SHRINK_SIZE:
-            # 'shrink' line by x meters in each end
-            p0 = g.interpolate(MARKER_SHRINK_SIZE)
-            p1 = g.interpolate (length - MARKER_SHRINK_SIZE)
-            coords = tocoordinates(g)
-            outcoords = []
-            for i in len(coords):
-                d = g.distanceToVertex(i)
-                if MARKER_SHRINK_SIZE < d < length - MARKER_SHRINK_SIZE:
-                    if not outcoords:
-                        outcoords.append(p0)
-                    outcoords.append(coords[i])
-            outcoords.append(p1)
-            shortedline = QgsGeometry.fromPolyline(outcoords)
-            return shortedline.nearestPoint(fromgeom)
-        else:
-            return g.centroid()
+        #length = g.length()
+        # if False and fromgeom and length > 2 * MARKER_SHRINK_SIZE:
+        #     # 'shrink' line by x meters in each end
+        #     p0 = g.interpolate(MARKER_SHRINK_SIZE).asPoint()
+        #     p1 = g.interpolate (length - MARKER_SHRINK_SIZE).asPoint()
+        #     coords = tocoordinates(g)
+        #     outcoords = []
+        #     d = 0.0
+        #     prev = coords[0]
+        #     for i in range(1, len(coords)):
+        #         curr = coords[i]
+        #         d += sqrt(prev.sqrDist(curr))
+        #         prev = curr
+        #         if MARKER_SHRINK_SIZE <= d <= length - MARKER_SHRINK_SIZE:
+        #             if not outcoords:
+        #                 outcoords.append(p0)
+        #             outcoords.append(coords[i])
+        #     outcoords.append(p1)
+        #     shortedline = QgsGeometry.fromPolyline(outcoords)
+        #     return shortedline.nearestPoint(fromgeom)
+        # else:
+        return g.centroid()
     elif g.type() == QGis.Polygon:
         if fromgeom:
             return g.buffer(-1 * MARKER_SHRINK_SIZE, 8).nearestPoint(fromgeom)
